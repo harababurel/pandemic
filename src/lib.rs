59983dc695d3f7928b6709054b0083f4a382d5b1
Sergiu Puscas <srg.pscs@gmail.com>
2023-01-14 20:55:37 +0100

    add infections, players, diseases

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,14 @@
 #![allow(unused_mut)]
 
 use rand::prelude::*;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 // use serde_json::Result;
+use colored::*;
 use std::collections::{HashMap, VecDeque};
 use std::fs;
 use thiserror::Error;
+
+const INFECTION_RATES: [i32; 7] = [2, 2, 2, 3, 3, 4, 4];
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -25,20 +28,40 @@ mod tests {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq, Eq, Default)]
 pub enum Color {
+    #[default]
     Blue,
     Yellow,
     Red,
     Black,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+impl Into<colored::Color> for Color {
+    fn into(self) -> colored::Color {
+        match self {
+            Color::Blue => colored::Color::Blue,
+            Color::Red => colored::Color::Red,
+            Color::Yellow => colored::Color::Yellow,
+            Color::Black => colored::Color::BrightBlack,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct City {
     name: String,
     color: Color,
     neighbors: Vec<String>,
     has_research_center: bool,
+    #[serde(default = "HashMap::new")]
+    infections: HashMap<Color, u32>,
+}
+
+impl std::fmt::Display for City {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.name.color(self.color.clone()))
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -47,7 +70,7 @@ pub enum PlayerCard {
     EpidemicCard,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum PlayerClass {
     Dispatcher,
     Generalist,
@@ -56,16 +79,64 @@ pub enum PlayerClass {
     Researcher,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Player {
     class: PlayerClass,
-    location: str,
+    location: String,
+    hand: Vec<PlayerCard>,
+}
+
+impl Player {
+    pub fn new(class: PlayerClass, location: &str) -> Self {
+        Player {
+            class: class,
+            location: location.to_string(),
+            hand: Vec::new(),
+        }
+    }
+
+    pub fn dispatcher(location: &str) -> Self {
+        Self::new(PlayerClass::Dispatcher, location)
+    }
+    pub fn generalist(location: &str) -> Self {
+        Self::new(PlayerClass::Generalist, location)
+    }
+    pub fn medic(location: &str) -> Self {
+        Self::new(PlayerClass::Medic, location)
+    }
+    pub fn scientist(location: &str) -> Self {
+        Self::new(PlayerClass::Scientist, location)
+    }
+    pub fn researcher(location: &str) -> Self {
+        Self::new(PlayerClass::Researcher, location)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Game {
     pub world: HashMap<String, City>,
     pub player_cards: VecDeque<PlayerCard>,
+    pub players: Vec<Player>,
+    pub infection_level: usize,
+    pub outbreaks: usize,
+    pub diseases: Vec<Disease>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq, Eq)]
+pub struct Disease {
+    pub color: Color,
+    pub cured: bool,
+    pub eradicated: bool,
+}
+
+impl Disease {
+    pub fn new(color: Color) -> Self {
+        Disease {
+            color,
+            cured: false,
+            eradicated: false,
+        }
+    }
 }
 
 impl Game {
@@ -73,7 +144,32 @@ impl Game {
         Game {
             world: HashMap::new(),
             player_cards: VecDeque::new(),
+            players: Vec::new(),
+            infection_level: 0,
+            outbreaks: 0,
+            diseases: vec![
+                Disease::new(Color::Blue),
+                Disease::new(Color::Red),
+                Disease::new(Color::Yellow),
+                Disease::new(Color::Black),
+            ],
         }
+    }
+
+    pub fn infection_rate(&self) -> i32 {
+        INFECTION_RATES[self.infection_level]
+    }
+
+    pub fn add_player(&mut self, p: Player) -> Result<(), PandemicError> {
+        if self.players.iter().any(|x| x.class == p.class) {
+            return Err(PandemicError::PlayerClassConflict);
+        }
+        if !self.world.contains_key(&p.location) {
+            return Err(PandemicError::InvalidPlayerlocation);
+        }
+
+        self.players.push(p);
+        Ok(())
     }
 
     pub fn load_cities(&mut self, path: &str) -> Result<(), PandemicError> {
@@ -135,4 +231,8 @@ pub enum PandemicError {
     SerdeJsonError(#[from] serde_json::Error),
     #[error("city graph error: {0}")]
     CityGraphError(String),
+    #[error("player class conflict")]
+    PlayerClassConflict,
+    #[error("invalid player location")]
+    InvalidPlayerlocation,
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,17 @@ fn main() {
         .expect("Could not parse cities.json");
     game.create_player_cards();
 
-    // println!("world: {:?}", world);
+    game.add_player(pandemic::Player::medic("Atlanta"))
+        .expect("Could not create medic in Atlanta");
+
+    game.add_player(pandemic::Player::scientist("Atlanta"))
+        .expect("Could not create medic in Atlanta");
+
+    println!("There are {} players", game.players.len());
     println!("There are {} cities", game.world.len());
     println!("There are {} player cards", game.player_cards.len());
+
+    for city in game.world.values() {
+        println!("{}", city);
+    }
 }
